bc7546419833a1d0f7916ffa683528d34388eb36
Dayanara847 <damsta1995@gmail.com>
2021-02-10 13:57:51 -0300

    Edit user is working

--- src/app/edit-user/edit-user.page.ts
+++ src/app/edit-user/edit-user.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
 import { LoadingController } from "@ionic/angular";
 import {
   FormGroup,
@@ -7,11 +6,10 @@ import {
   FormBuilder,
   Validators,
 } from "@angular/forms";
-import { User } from "src/app/models/user.model";
-import { AccountService } from "src/app/services/account.service";
 import { HttpClient } from "@angular/common/http";
 import { Endpoints } from "../services/endpoints";
 import { Storage } from "@ionic/storage";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-edit-user",
@@ -33,13 +31,12 @@ export class EditUserPage implements OnInit {
   });
 
   constructor(
-    private router: Router,
     public loadingController: LoadingController,
     private formBuilder: FormBuilder,
-    private accountService: AccountService,
     private http: HttpClient,
     private endpoints: Endpoints,
-    private storage: Storage
+    private storage: Storage,
+    private router:Router, 
   ) {}
 
   ngOnInit() {
@@ -52,17 +49,6 @@ export class EditUserPage implements OnInit {
     });
   }
 
-/*   onSubmitEditUser() {
-    const formValue = this.profileForm.value;
-    const userEdited = {
-      firstName: formValue["firstName"],
-      lastName: formValue["lastName"],
-      birthdate: formValue["birthdate"],
-      cellphone: formValue["cellphone"],
-      photo: formValue["photo"]
-    }
-  } */
-
   onSubmit() {
     const formValue = this.profileForm.value;
     const userEdited = {
@@ -70,11 +56,20 @@ export class EditUserPage implements OnInit {
       lastName: formValue["lastName"],
       birthdate: formValue["birthdate"],
       cellphone: formValue["cellphone"],
-      photo: formValue["photo"]
-    }
-    this.http.put(this.endpoints.EDIT_USER_ENDPOINT + "/" + this.id, userEdited)
-    .subscribe((resp) => {
-      return resp;
-      })
+      photo: formValue["photo"],
+    };
+    return new Promise((resolve) => {
+      this.http
+        .put(this.endpoints.EDIT_USER_ENDPOINT + "/" + this.id, userEdited)
+        .subscribe(
+          (resp) => {
+            this.router.navigate(['tabs/account'])
+            return resp;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    });
   }
 }
