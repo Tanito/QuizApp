45af14ea22e3bffadba7f57bce98e63b43ca677e
Tanito <ulises.d83@gmail.com>
2021-02-11 17:06:53 -0300

    alert msg when enroll

--- src/app/pages/orgs/orgs.page.ts
+++ src/app/pages/orgs/orgs.page.ts
@@ -10,110 +10,127 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./orgs.page.scss'],
 })
 export class OrgsPage implements OnInit {
-      Quizzes: any = {};
-      city: string;
-  ​    country: string;
-  ​    description:string;
-  ​    email: string;
-  ​    id: number;
-  ​    logo: string;
-  ​    name: string;
-      schoolQuizzes: any;
-      schoolQuizzesBU: any;
-      body: any;
-      userId: number;
-      quizId: number;
-      info: any;
+  Quizzes: any = {};
+  city: string;
+  country: string;
+  description: string;
+  email: string;
+  id: number;
+  logo: string;
+  name: string;
+  schoolQuizzes: any;
+  schoolQuizzesBU: any;
+  body: any;
+  userId: number;
+  quizId: number;
+  info: any;
 
-  constructor( private storage: Storage,
-    private http: HttpClient, 
+  constructor(private storage: Storage,
+    private http: HttpClient,
     private endpoints: Endpoints,
     public alertController: AlertController,) { }
 
   ngOnInit() {
-    this.cargarStorage().then(() =>{
+    this.cargarStorage().then(() => {
 
       this.getOrgQuizzes(this.id)
-      .subscribe(async resp => {
-       this.Quizzes = await resp
-       this.schoolQuizzes = this.Quizzes.quizzes.byId
-       this.schoolQuizzesBU = this.Quizzes.quizzes.byId 
-        // let listado = resp
-        console.log("listado", this.schoolQuizzes)
-      
-      })
+        .subscribe(async resp => {
+          this.Quizzes = await resp
+          this.schoolQuizzes = this.Quizzes.quizzes.byId
+          this.schoolQuizzesBU = this.Quizzes.quizzes.byId
+          // let listado = resp
+          console.log("listado", this.schoolQuizzes)
+
+        })
     })
   }
 
-  getOrgQuizzes(id){
+  getOrgQuizzes(id) {
     console.log("se ejecuta get quizzes", this.endpoints.SCHOOL_ENDPOINT + '/' + id + '/quizzes')
     return this.http.get(this.endpoints.SCHOOL_ENDPOINT + '/' + id + '/quizzes')
-    
-  } 
+
+  }
 
   searchInputChanged(searchOrg) {
     console.log(this.schoolQuizzesBU)
-      if (searchOrg === '') {
-       
-       this.schoolQuizzes = this.schoolQuizzesBU
-      
-       return }
-       if(searchOrg !== '')  this.schoolQuizzes = this.schoolQuizzesBU.filter((o) => {
-         
-          
-             return (o.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(searchOrg.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) );
-         })
- 
-        
- }
+    if (searchOrg === '') {
 
- async presentAlert(name) {
-  const alert = await this.alertController.create({
-    cssClass: 'my-custom-class',
-    header: 'QuizApp',
-    // subHeader: 'Subtitle',
-    message: 'Ya eres '+ name +' de este quiz!',
-    buttons: ['OK']
-  });
+      this.schoolQuizzes = this.schoolQuizzesBU
 
-  await alert.present();
-}
+      return
+    }
+    if (searchOrg !== '') this.schoolQuizzes = this.schoolQuizzesBU.filter((o) => {
 
-  addToFavs(userId, quizId){
-    const body = {UserId: userId, QuizId: quizId }
-    console.log("UserID",userId,"QuizId", quizId)
+
+      return (o.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(searchOrg.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")));
+    })
+
+
+  }
+
+  async presentAlert(name) {
+    switch (name) {
+      case "Student":
+        name = "Estudiante"
+        break;
+      case "Teacher":
+        name = "Profesor"
+        break;
+      case "Enrolled":
+        name = "Aspirante"
+        break;
+      case "Fan":
+        name = "Seguidor"
+        break;
+    }
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'QuizApp',
+      // subHeader: 'Subtitle',
+      message: 'Ya eres ' + name + ' de este quiz!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  addToFavs(userId, quizId) {
+    const body = { UserId: userId, QuizId: quizId }
+    console.log("UserID", userId, "QuizId", quizId)
     this.http.post(this.endpoints.ADD_TO_FAVS_ENDPOINT, body).subscribe(data => {
       this.info = data;
       this.presentAlert(this.info.name)
-      console.log("INFO",this.info)
-     })
+    
+    })
   }
 
-//Probar si funciona el enroll.
-  inscriptionQuizApi(userId, quizId){
-    console.log("UserID",userId,"QuizId", quizId)
-    const body = {UserId: userId, QuizId: quizId }
+  //Probar si funciona el enroll.
+  inscriptionQuizApi(userId, quizId) {
+    console.log("UserID", userId, "QuizId", quizId)
+    const body = { UserId: userId, QuizId: quizId }
     this.http.post(this.endpoints.JOIN_QUIZ_ENDPOINT, body).subscribe(data => {
       this.info = data;
-     }
- )
-   
+      this.presentAlert(this.info.name)
+      console.log("INFO", this.info)
+    }
+    )
+
   }
 
-  async cargarStorage(){ //Cargo el localStorage
+  async cargarStorage() { //Cargo el localStorage
     await this.storage.get('School').then(val => {
-     this.id = val.id; 
-     this.name= val.name;
-     this.email= val.email;
-     this.logo= val.logo;
-     this.description= val.description;    
-     this.country= val.country;
-     this.city= val.city;
-     console.log("id2", this.id)
-   })
-   this.storage.get('User').then(val => { 
-    this.userId= val.user.id;
-   })
-   }
+      this.id = val.id;
+      this.name = val.name;
+      this.email = val.email;
+      this.logo = val.logo;
+      this.description = val.description;
+      this.country = val.country;
+      this.city = val.city;
+      console.log("id2", this.id)
+    })
+    this.storage.get('User').then(val => {
+      this.userId = val.user.id;
+    })
+  }
 
 }
