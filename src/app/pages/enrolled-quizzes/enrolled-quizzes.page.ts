b17680e6448ed9356500f595432e89a3f68d896d
Tanito <ulises.d83@gmail.com>
2021-02-11 12:05:34 -0300

    render empty list when there's no items

--- src/app/pages/enrolled-quizzes/enrolled-quizzes.page.ts
+++ src/app/pages/enrolled-quizzes/enrolled-quizzes.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Endpoints } from "../../services/endpoints";
 import { Storage } from "@ionic/storage";
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: "app-enrolled-quizzes",
@@ -10,17 +13,29 @@ import { Storage } from "@ionic/storage";
 })
 export class ProfileEnrolledQuizzesPage implements OnInit {
   EnrolledQuizzes: any;
+  vacio: boolean = false;
   id: number;
   info: any;
-
+  quizId: number;
+  
   constructor(
     private http: HttpClient,
     private endpoints: Endpoints,
-    private storage: Storage
+    private storage: Storage,
+    public alertController: AlertController,
+    private router: Router,
   ) {}
 
   ngOnInit() {
     this.cargarStorage();
+  }
+
+  ionViewWillEnter() {
+    this.cargarStorage();
+    // console.log("Favoritos", this.Favorites, this.vacio)
+    // console.log("PRUEBA", (this.Favorites === undefined))
+    if (this.EnrolledQuizzes === undefined) return this.vacio === true;
+    if (this.EnrolledQuizzes !== undefined) return this.vacio === false;
   }
 
   cargarStorage() {
@@ -33,11 +48,13 @@ export class ProfileEnrolledQuizzesPage implements OnInit {
         this.getEnrolledQuizzes(this.id).subscribe( resp => {
           console.log('resp', resp)
           this.EnrolledQuizzes = resp
-         })
+          if (this.EnrolledQuizzes.length === 0) {this.vacio = true }
+        })
+         
       });
   }
 
-  remove(userId, quizId){
+  remove(userId, quizId, name){
     console.log("user & quiz ids", userId, quizId)
     const body = {UserId: userId, QuizId: quizId }
     console.log("UserID",userId,"QuizId", quizId)
@@ -46,8 +63,22 @@ export class ProfileEnrolledQuizzesPage implements OnInit {
       this.info = data;
       console.log("info devuelta", this.info)
      })
+     this.presentAlert(name)
+     this.router.navigateByUrl('/tabs/enrolled-quizzes')
+     this.ionViewWillEnter()
   }
 
+  async presentAlert(name) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'QuizApp',
+      // subHeader: 'Subtitle',
+      message: 'Ha retirado la inscripción del quiz '+ name +'!',
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
   getEnrolledQuizzes(id) {
     this.id = id;
     return this.http.get(this.endpoints.ENROLLED_ENDPOINT + "/" + this.id);
--- src/app/pages/profile-favs/profile-favs.page.ts
+++ src/app/pages/profile-favs/profile-favs.page.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Endpoints } from "../../services/endpoints";
 import { Storage } from "@ionic/storage";
 import { TabsPage } from '../tabs/tabs.page';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-profile-favs",
@@ -12,9 +14,11 @@ import { TabsPage } from '../tabs/tabs.page';
 })
 export class ProfileFavsPage implements OnInit {
   Favorites: any;
+  vacio: boolean = false;
   id: number;
   info: any;
   quizId: number;
+  quizName: string;
 
   constructor(
     private docsService: DocsService,
@@ -22,10 +26,20 @@ export class ProfileFavsPage implements OnInit {
     private endpoints: Endpoints,
     private storage: Storage,
     private tabsPage: TabsPage,
+    public alertController: AlertController,
+    private router: Router,
   ) {}
 
   ngOnInit() {
+     this.cargarStorage();
+  }
+
+  ionViewWillEnter() {
     this.cargarStorage();
+    console.log("Favoritos", this.Favorites, this.vacio)
+    // console.log("PRUEBA", (this.Favorites === undefined))
+    if (this.Favorites === undefined) return this.vacio === true;
+    if (this.Favorites !== undefined) return this.vacio === false;
   }
 
   cargarStorage() {
@@ -37,6 +51,7 @@ export class ProfileFavsPage implements OnInit {
       .then((f) => {
         this.getFavorites(this.id).subscribe( resp => {
           this.Favorites = resp
+          if (this.Favorites.length === 0) {this.vacio = true }
          })
       });
   }
@@ -46,7 +61,7 @@ export class ProfileFavsPage implements OnInit {
     return this.http.get(this.endpoints.FAVORITES_ENDPOINT + "/" + this.id);
   }
 
-  remove(userId, quizId){
+  remove(userId, quizId, name){
     console.log("user & quiz ids", userId, quizId)
     const body = {UserId: userId, QuizId: quizId }
     console.log("UserID",userId,"QuizId", quizId)
@@ -55,6 +70,21 @@ export class ProfileFavsPage implements OnInit {
       this.info = data;
       console.log("info devuelta", this.info)
      })
+     this.presentAlert(name)
+     this.router.navigateByUrl('/tabs/profile-favs')
+     this.ionViewWillEnter()
+  }
+
+  async presentAlert(name) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'QuizApp',
+      // subHeader: 'Subtitle',
+      message: 'El quizz '+ name +' se quitó de favoritos!',
+      buttons: ['OK']
+    });
+  
+    await alert.present();
   }
 
   openFirst(){
