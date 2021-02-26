40908c95529dd2b934fa9e80220ba16f890ff4f0
Tanito <ulises.d83@gmail.com>
2021-02-26 17:18:11 -0300

    login face working (only login)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
 import { UsuarioService } from "../services/usuario.service";
+import { AuthService } from '../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { Endpoints } from "../services/endpoints";
 //Firebase
 import { AngularFireAuth } from '@angular/fire/auth';
 import { NavController } from '@ionic/angular';
@@ -13,12 +18,56 @@ import firebase from 'firebase/app';
 })
 export class HomePage {
 
+  password: string;
+  email: string;
+  userObject: any;
+  loginError: string;
+
 
   constructor(private afAuth: AngularFireAuth,
-    public usuarioProv: UsuarioService,
-    private navCtrl: NavController) {
+              public usuarioProv: UsuarioService,
+              private navCtrl: NavController,
+              private authService: AuthService,
+              private http: HttpClient,
+              private router: Router,
+              private storage: Storage,
+              private endpoints: Endpoints) {
 
   }
+
+  imprimir(){
+    console.log(this.email, this.password)
+  };
+
+
+  loginOK() {
+    this.storage.set('User', this.userObject);
+   setTimeout( () => {
+    this.router.navigate(['tabs'])
+  }, 2000);
+    ;
+  }
+
+  loginFunction (){
+    console.log("DATOS1",this.email, this.password)
+      this.authService.login()
+    }
+
+    login(){
+      const body = {email: this.email,
+      password: this.password}
+
+     this.http.post(this.endpoints.LOGIN_ENDPOINT, body).subscribe(data => {
+          this.userObject = data;
+          if (this.userObject.token !== undefined){
+            this.loginOK()
+         }
+         }, error => {
+          this.loginError = "Usuario o Password incorrectos"
+          
+   } 
+     )};
+
   signInWithFacebook() {
     this.afAuth
       .signInWithPopup(new firebase.auth.FacebookAuthProvider())
@@ -34,7 +83,7 @@ export class HomePage {
           'facebook'
         );
 
-        this.navCtrl.navigateRoot( '/home' )
+        this.navCtrl.navigateRoot('/home')
       });
   }
 
